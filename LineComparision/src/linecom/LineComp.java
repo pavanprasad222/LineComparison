fc958f5f92f29b7639e8615be21f8203609b6024
pavan sriram prasad <pittapavansriramprasad@gmailcom>
2021-05-08 16:57:02 +0530

    Welcome message

---
+++ LineComparision/src/linecom/LineComp.java
@@ -0,0 +1,11 @@
+package linecom;
+import java.util.*;
+
+public class LineComp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Welcome To Line Comparison Computational Program");
+	}
+
+}
